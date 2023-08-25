f3c622d0b976f8533e0b39307878d3f330fe21f2
longpham2511 <longpham2511@gmail.com>
2023-08-25 17:54:09 +0700

    OOP_training

--- JV_BASIC/src/jv_stream/jv_stream_list.java
+++ JV_BASIC/src/jv_stream/jv_stream_list.java
@@ -1,7 +1,17 @@
 package jv_stream;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class jv_stream_list {
     public static void main(String[] args) {
+
+        //average_calculator
+            List<Integer> numlist = Arrays.asList(5, 20, 30, 30, 15);
+            System.out.println("The number list is " + numlist);
+            double average = numlist.stream().mapToDouble(Integer::doubleValue).average().orElse(0.0);
+            System.out.println("Average of the list is " + average);
+
 
     }
 
