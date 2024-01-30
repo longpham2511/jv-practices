63b0fab27bcc1ca9a6c6eb7d9159591645dfea1d
Long Pham <longpham2511@gmail.com>
2024-01-30 23:56:24 +0700

    Selenium Practice: (1/30/24) - exercise practise: + testNG_functionalities

--- JV_BASIC/Selenium_Udemy/src/main/java/Selenium_Framework_TestNG/testNG_Framework1.java
+++ JV_BASIC/Selenium_Udemy/src/main/java/Selenium_Framework_TestNG/testNG_Framework1.java
@@ -6,7 +6,25 @@ import org.testng.annotations.Test;
 public class testNG_Framework1 {
     /*
     TestNG acts as a Java compiler, instead of putting psvm-main method, testng can replace it
-    Need to have @Test annotation to run
+    Need to have @Test annotation followed by a method to run
+    can define multiple tests from a single class
+    can modularize the testcase based on functionality and trigger them accordingly
+    u can get control running a specific test case
+    Test suite -> Test folder (module) -> test cases
+    test case (method) < class (module) < test < suite
+    @BeforeTest/AfterTest -
+    @beforeSuite/AfterSuite - declare globally
+    @BeforeMethod/AfterMethod - Beforemethod - method 1 - aftermethod - beforemethod - method 2 - aftermethod -....
+    @Beforeclass/Afterclass
+    @Test(groups={"<<GroupName>>"})
+
+    Test cases will be run alphabetical order without annotations
+
+    Interview questions:
+    - Testng - timeout - to wait for a period of time before giving error
+             - enabled = false/ true - to disable a test case and run the test suite
+             - @test(group={"hehe"}) - give tc tags to run them separately
+             - @test(dependsonMethod={"method1"}} - this dependent method will be executed after the mentioned tc
      */
     @Test
     public void demo1(){
@@ -17,5 +35,6 @@ public class testNG_Framework1 {
     public void demo2(){
         System.out.println("Bye World");
     }
+
 
 }
--- JV_BASIC/Selenium_Udemy/src/main/java/Selenium_Framework_TestNG/testNG_extra.java
+++ JV_BASIC/Selenium_Udemy/src/main/java/Selenium_Framework_TestNG/testNG_extra.java
@@ -1,25 +1,64 @@
 package Selenium_Framework_TestNG;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class testNG_extra {
-    @Test
-    public void demo_extra(){
-        System.out.println("Hello from the extra side");
+    @BeforeSuite //used this annotation to declare something globally - i.e. declare url
+    public void demo_extra() {
+        System.out.println("Bforesuite message");
     }
-    @Test
-    public void demo_extra1(){
-        System.out.println("Hello from the extra side 1");
+
+    @AfterSuite //used this annotation to declare something globally - i.e. clear cache
+    public void demo_extra1() {
+        System.out.println("Aftersuite message");
     }
 
     @Test
-    public void extra1(){
+    public void extra1() {
         System.out.println("Hello from the EXTRA");
     }
-@BeforeTest
-    public void extra2(){
+
+    @BeforeTest
+    public void extra2() {
         System.out.println("Hello from the EXTRA 2 - this is a before test annotation");
     }
+
+    @Test(groups = {"grouptest"}) //put these test into a group
+    public void group1() {
+        System.out.println("Group Testing 1");
+    }
+
+    @Test(groups = {"grouptest"})
+    public void group2() {
+        System.out.println("Group Testing 2");
+    }
+
+    @Test(dependsOnMethods = {"tc_independ"})
+    public void tc_depend1() {
+        System.out.println("The depend test case will run after the independ one");
+    }
+
+    @Test
+    public void tc_independ() {
+        System.out.println("independ test case");
+    }
+    @Test(dependsOnMethods = {"tc_independ","tc_depend"})
+    public void tc_depend2() {
+        System.out.println("The 2nd depend test case will run after the depend and independ1 tc");
+    }
+
+    @Test(enabled = false) //can leave at true to run it again
+    public void skip_tc() {
+        System.out.println("you can skip this test case due to known issues");
+    }
+
+    @Test(timeOut = 5000) //if that test case takes a long time to finish, leave the timeout so it will wait and not throw any errors
+    public void time_out() {
+        System.out.println("time_out");
+    }
+
 }
 
